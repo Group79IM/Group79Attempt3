49c4441dc09a21b2a041b57f0a5883bf7afc520f
Flighless <146098168+Flighless@users.noreply.github.com>
2025-05-27 01:53:15 +0100

    Exit Button Works

--- Assets/Scripts/GameReset.cs
+++ Assets/Scripts/GameReset.cs
@@ -9,6 +9,9 @@ public class GameReset : MonoBehaviour
     [SerializeField] private GameObject shopObject;
     [SerializeField] private Interactions interactions;
     [SerializeField] private ShopScript shopScript;
+    [SerializeField] private GameObject tutorialObject;
+    [SerializeField] private GameObject buttonContainer;
+    [SerializeField] private GameObject menuBacking;
     private Money moneyScript;
 
     void Awake()
@@ -16,6 +19,9 @@ public class GameReset : MonoBehaviour
         moneyScript = moneyObject.GetComponent<Money>();
         shopScript = shopObject.GetComponent<ShopScript>();
         GameReseting();
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     void GameReseting()
@@ -25,5 +31,28 @@ public class GameReset : MonoBehaviour
         shopScript.playerBoughtSword = false;
         shopScript.playerBoughtSwordTwo = false;
         shopScript.playerBoughtSwordThree = false;
+    }
+
+    public void EgyptScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void TutorialScene()
+    {
+        buttonContainer.SetActive(false);
+        menuBacking.SetActive(false);
+        tutorialObject.SetActive(true);
+    }
+    public void CloseTutorial()
+    {
+        tutorialObject.SetActive(false);
+        buttonContainer.SetActive(true);
+        menuBacking.SetActive(true);
+    }
+    public void QuitApp()
+    {
+        // AudioSource.PlayClipAtPoint(buttonClick, transform.position, 1f);
+        Application.Quit();
+        Debug.Log("Application Has Quit");
     }
 }
